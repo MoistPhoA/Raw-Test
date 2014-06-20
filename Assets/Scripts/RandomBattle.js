6a20dff37395f64bc4e5ec8822f9d56405d8dd28
MoistPhoA <mugennights@yahoo.com>
2014-06-20 18:57:46 -0400

    Version 1 test

---
+++ Assets/Scripts/RandomBattle.js
@@ -0,0 +1,25 @@
+﻿#pragma strict
+static var inBattleCamera = false ;
+
+static var battleMode = false ;
+static var randomNumber = 0;
+var snap = false;
+
+function OnTriggerEnter(info: Collider) { 
+		snap = true ;
+		Debug.Log ("snap is true") ;
+		if (snap == true){
+		InvokeRepeating ("Timer", 2, 3) ; 
+		// invoke repeating -> ("function", delay in seconds before the function executes, delay before repeat execution)
+			}
+	}
+
+function Timer () {
+	Debug.Log ("Timer function was called and ran at least once") ;
+	randomNumber = Random.Range(1,101) ;
+		if (randomNumber < 11 && randomNumber > 1) {
+			battleMode = true ;	
+			Debug.Log ("random number is < 11 and > 1" ) ;
+			Application.LoadLevel ("battle") ;
+			}
+}
---
+++ Assets/Scripts/Spawner.js
@@ -0,0 +1,36 @@
+﻿#pragma strict
+var capsule : Transform ;
+var cylinder : Transform ;
+var oriCapsule : GameObject ;
+var oriCylinder : GameObject ;
+Invoke ("spawner", 1) ;
+function spawner (){
+	switch (Random.Range( 1, 4 ) ) {
+		case 1 : 
+			Debug.Log("case 1 initiated") ;
+			Instantiate (capsule , Vector3(1.5, 1, 0), Quaternion.identity) ;
+			capsule.tag = "clone" ;
+			oriCapsule.SetActive(false) ;
+			oriCylinder.SetActive(false) ;
+			break;
+		case 2 :
+			Debug.Log("case 2 initiated") ;
+			Instantiate (cylinder , Vector3(3, 1, 0), Quaternion.identity) ;
+			cylinder.tag = "clone" ;
+			oriCylinder.SetActive(false) ;
+			oriCapsule.SetActive(false) ;
+			break ;
+		case 3 :
+			Debug.Log("case 3 initiated") ;
+			Instantiate (capsule , Vector3(1.5, 1, 0), Quaternion.identity) ;
+			capsule.tag = "clone" ;
+			Instantiate (cylinder , Vector3(3, 1, 0), Quaternion.identity) ;
+			cylinder.tag = "clone" ;
+			oriCapsule.SetActive(false) ;
+			oriCylinder.SetActive(false) ;
+			break ;
+		default :
+			print ("Spawning error") ;
+			break ;
+	} 
+}
